c7f9ea028686d1fcad88350e2630334a644cd4b2
originSoftwareHyd <originsoftwareshyd@gmail.com>
2026-01-28 15:54:18 +0530

    New project

---
+++ Quote Generator/script.js
@@ -0,0 +1,46 @@
+// Class to manage Quotes
+class QuoteApp {
+  constructor() {
+    this.quoteEl = document.getElementById("quote");
+    this.authorEl = document.getElementById("author");
+    this.btn = document.getElementById("btn");
+
+    this.apiUrl = "https://api.quotable.io/random";
+
+    this.btn.addEventListener("click", () => this.getQuote());
+  }
+
+  // Async function using fetch API
+  async getQuote() {
+    try {
+      this.showLoading();
+
+      const response = await fetch("https://dummyjson.com/quotes/random");
+      const data = await response.json();
+
+      const quoteObject = {
+        content: data.quote,
+        author: data.author,
+      };
+
+      this.displayQuote(quoteObject);
+    } catch (error) {
+      console.error(error);
+      this.quoteEl.textContent = "Failed to load quote 😢";
+      this.authorEl.textContent = "";
+    }
+  }
+
+  showLoading() {
+    this.quoteEl.textContent = "Loading...";
+    this.authorEl.textContent = "";
+  }
+
+  displayQuote(quote) {
+    this.quoteEl.textContent = `"${quote.content}"`;
+    this.authorEl.textContent = `— ${quote.author}`;
+  }
+}
+
+// Create object from class
+const app = new QuoteApp();
